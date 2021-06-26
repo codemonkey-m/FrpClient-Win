a2384251a71d068fa4ba7631585f2619a6de6fd9
Ming <gamesoftware2010@gmail.com>
2021-06-26 14:56:28 +0800

    1.修改版本号

--- FrpClient-Win/Properties/AssemblyInfo.cs
+++ FrpClient-Win/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.1.4")]
+[assembly: AssemblyFileVersion("0.0.1.5")]
